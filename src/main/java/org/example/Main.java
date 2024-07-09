a11d167613212a578e54bdbb9fa2bab18f3b4d8c
潘吉祥 <1121153008@qq.com>
2024-07-09 17:43:19 +0800

    a

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+/**
+ * @auther qiemengyan
+ * @date 2024/7/9
+ * @desc <p>
+ *
+ * </p>
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
